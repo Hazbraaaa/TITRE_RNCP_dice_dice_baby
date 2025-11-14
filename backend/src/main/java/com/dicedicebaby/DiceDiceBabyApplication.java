045f1f865df9d68ccbf980a9a3d0228dbd5c6f27
chazera <hazera.c@gmail.com>
2025-11-14 11:57:42 +0100

    add: Monorepo, recover backend from old repo (no model or anything)

---
+++ backend/src/main/java/com/dicedicebaby/DiceDiceBabyApplication.java
@@ -0,0 +1,13 @@
+package com.dicedicebaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceDiceBabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceDiceBabyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dicedicebaby/DiceDiceBabyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicedicebaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceDiceBabyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
